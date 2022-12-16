6b376beb0b8acadc838c232241f27ad2e644ed9b
didriknilsson <didrik.nilsson@iths.se>
2022-12-16 12:55:45 +0100

    test

--- CleanCodeLabUnitTest/MooGameUnitTest.cs
+++ CleanCodeLabUnitTest/MooGameUnitTest.cs
@@ -7,11 +7,25 @@ namespace CleanCodeLabUnitTest
     [TestClass]
     public class MooGameUnitTest
     {
-        MooGame mooGame = new MooGame();
-        [TestMethod()]
-        public void MooGameTargetToGuessIsFourDigits()
-        {
-            StringAssert.Matches(mooGame.CreateTargetToGuess(), new Regex(@"^\d{4}$"));
-        }
+        //MooGame? mooGame;
+        //string playerGuess;
+        //string targetToGuess;
+        //[TestInitialize]
+        //public void MooGameTestInitialize()
+        //{
+        //    mooGame = new MooGame();
+        //    playerGuess = "1234";
+        //    targetToGuess = "1256";
+        //}
+        //[TestMethod()]
+        //public void MooGameTargetToGuessIsFourDigits()
+        //{
+        //    StringAssert.Matches(mooGame?.CreateTargetToGuess(), new Regex(@"^\d{4}$"));
+        //}
+        //[TestMethod()]
+        //public void MooGameCheckedGuessReturnsBOrC()
+        //{
+        //    StringAssert.Matches(mooGame?.CheckPlayerGuess(playerGuess, targetToGuess), new Regex(@"^[BC\,]+$"));
+        //}
     }
 }
